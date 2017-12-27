647abc4f977e14c30592c9b4c8f43d9830c4b1b7
xiaogede <2469136989@qq.com>
2017-12-27 19:31:34 +0800

    yaoqinghan

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: false,
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
